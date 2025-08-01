35f790734b2a0f950045055ba0b2ed2586c1c8ad
Vyacheslav Shengur <slavcho21@gmail.com>
2025-08-01 22:01:35 +0300

    Fixed DTO

--- src/Application/DTOs/BookingDto.cs
+++ src/Application/DTOs/BookingDto.cs
@@ -6,10 +6,10 @@ namespace BookingService.Application.DTOs
     {
         public Guid Id { get; init; }
         public Guid RoomId { get; init; }
-        public Guid UserId { get; init; }
+        public Guid GuestId { get; init; }
         public DateOnly CheckIn { get; init; }
         public DateOnly CheckOut { get; init; }
-        public decimal TotalPrice { get; init; }
+        public string TotalPrice { get; init; }
         public string Status { get; init; } = default!;
     }
 }
--- src/Application/Profiles/BookingProfile.cs
+++ src/Application/Profiles/BookingProfile.cs
@@ -11,7 +11,8 @@ namespace BookingService.Application.Profiles
             CreateMap<Booking, BookingDto>()
                 .ForMember(d => d.CheckIn, opt => opt.MapFrom(s => s.CheckInDate))
                 .ForMember(d => d.CheckOut, opt => opt.MapFrom(s => s.CheckOutDate))
-                .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Status.ToString()));
+                .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Status.ToString()))
+                .ForMember(d => d.TotalPrice, opt => opt.MapFrom(s => $"{s.TotalPrice.Amount} {s.TotalPrice.Currency}"));
         }
     }
 }
